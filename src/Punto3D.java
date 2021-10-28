35714ad0dda1f48d147b5f2aa20d371933be5d7e
_Ontos_ <josemercado.musica@gmail.com>
2021-10-28 15:01:35 -0300

    Creé los métodos de la clase Punto3D. Creé método adicional para las distancias entre puntos

--- src/Punto3D.java
+++ src/Punto3D.java
@@ -22,16 +22,28 @@ public class Punto3D {
     }
 
     public double distanciaDesdeOrigen() {
-
-        return 0;
+        double distancia;
+        // distancia = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2));
+        distancia = distanciaEntrePuntos(this, new Punto3D(0, 0, 0));
+        return distancia;
     }
 
     public double distanciaDesdeOtroPunto(double x, double y, double z) {
-
-        return 0;
+        double distancia;
+        distancia = distanciaEntrePuntos(this, new Punto3D(x, y, z));
+        return distancia;
     }
 
     public double distanciaDesdeOtroPunto(Punto3D otroPunto) {
-        return 0;
+        double distancia;
+        distancia = distanciaEntrePuntos(this, otroPunto);
+        return distancia;
+    }
+
+    // Método adicional para no repetir la fórmula matemática de distancia | DRY (Don't repeat yourself)
+    public static double distanciaEntrePuntos(Punto3D punto1, Punto3D punto2) {
+        double distancia;
+        distancia = Math.sqrt(Math.pow(punto1.x - punto2.x, 2) + Math.pow(punto1.y - punto2.y, 2) + Math.pow(punto1.z - punto2.z, 2));
+        return distancia;
     }
 }
